c6ab64d935f3107388d49d11544e9ab496a0555b
Hadrien G. <knights_of_ni@gmx.com>
2024-05-02 07:23:26 +0200

    Add tests for ValiditySlice

--- src/validity.rs
+++ src/validity.rs
@@ -114,16 +114,92 @@ impl<'bytes> Iterator for BitmapIter<'bytes> {
     #[inline]
     fn next(&mut self) -> Option<bool> {
         // Access the current bit
-        let byte = self.current_byte?;
+        let byte = &self.current_byte?;
         let result = (byte & self.bit) != 0;
 
         // Move to the next byte/bit
         self.bit = self.bit.wrapping_shl(1);
-        if self.bit == 1 {
+        if self.bit == 0 {
             self.current_byte = self.bytes.next().copied();
+            self.bit = 1;
         }
 
         // Return the result
         Some(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+
+    /// Generate validity bitmap building blocks
+    fn building_blocks() -> impl Strategy<Value = (Vec<u8>, usize)> {
+        prop_oneof![
+            any::<Vec<bool>>().prop_map(|bits| { bits_to_bitmap(&bits) }),
+            any::<(Vec<u8>, usize)>()
+        ]
+    }
+
+    /// Convert validity bits into validity bitmap building blocks
+    fn bits_to_bitmap(bits: &[bool]) -> (Vec<u8>, usize) {
+        let array_len = bits.len();
+        let bytes = bits
+            .chunks(8)
+            .map(|chunk| {
+                let mut byte = 0u8;
+                for (idx, bit) in chunk.iter().enumerate() {
+                    byte |= (*bit as u8) << idx;
+                }
+                byte
+            })
+            .collect::<Vec<_>>();
+        (bytes, array_len)
+    }
+
+    proptest! {
+        #[test]
+        fn init((bitmap, array_len) in building_blocks()) {
+            let res = std::panic::catch_unwind(|| ValiditySlice::new(&bitmap, array_len));
+
+            if bitmap.len() != array_len.div_ceil(8) {
+                prop_assert!(res.is_err());
+                return Ok(());
+            }
+            let validity = res.unwrap();
+
+            prop_assert_eq!(validity.len(), array_len);
+            prop_assert_eq!(validity.is_empty(), array_len == 0);
+            prop_assert_eq!(validity.iter().count(), array_len);
+            for (idx, bit) in validity.iter().enumerate() {
+                prop_assert_eq!(bit, bitmap[idx / 8] & (1 << (idx % 8)) != 0);
+            }
+        }
+    }
+
+    /// Generate a validity bitmap, its unpacked bits, and an index into it
+    fn bitmap_bits_index() -> impl Strategy<Value = ((Vec<u8>, usize), Vec<bool>, usize)> {
+        let bits = any::<Vec<bool>>();
+        bits.prop_flat_map(|bits| {
+            let bitmap = Just(bits_to_bitmap(&bits));
+            let index = prop_oneof![0..=bits.len(), bits.len()..];
+            (bitmap, Just(bits), index)
+        })
+    }
+
+    proptest! {
+        #[test]
+        fn index(((bitmap, array_len), bits, index) in bitmap_bits_index()) {
+            let in_bounds = index < bits.len();
+            let validity = ValiditySlice::new(&bitmap, array_len);
+
+            prop_assert_eq!(validity.get(index), bits.get(index).copied());
+            let index_res = std::panic::catch_unwind(|| validity.at(index));
+            prop_assert_eq!(index_res.ok(), bits.get(index).copied());
+            if in_bounds {
+                prop_assert_eq!(unsafe { validity.get_unchecked(index) }, bits[index]);
+            }
+        }
     }
 }
