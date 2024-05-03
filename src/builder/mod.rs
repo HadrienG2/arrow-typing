3665945b7cf6b60d9accd6bf4c048c895a5dff3e
Hadrien G. <knights_of_ni@gmx.com>
2024-05-03 11:00:24 +0200

    Basic examples in docs

--- src/builder/mod.rs
+++ src/builder/mod.rs
@@ -4,7 +4,7 @@ pub(crate) mod backend;
 
 use self::backend::{Backend, ExtendFromSlice, TypedBackend};
 #[cfg(doc)]
-use crate::types::primitive::PrimitiveType;
+use crate::{types::primitive::PrimitiveType, OptionSlice};
 use crate::{validity::ValiditySlice, ArrayElement, NullableElement, SliceElement};
 use arrow_array::builder::ArrayBuilder;
 use std::fmt::{self, Debug, Formatter};
@@ -22,11 +22,21 @@ where
     BackendConfig<T>: Default,
 {
     /// Create a new array builder with the default configuration
+    ///
+    /// ```rust
+    /// # use arrow_typing::TypedBuilder;
+    /// let builder = TypedBuilder::<bool>::new();
+    /// ```
     pub fn new() -> Self {
         Self(BuilderBackend::<T>::new(BuilderConfig::<T>::new()))
     }
 
-    /// Create a new array builder with space for `capacity` elements
+    /// Create a new array builder with space for at least `capacity` elements
+    ///
+    /// ```rust
+    /// # use arrow_typing::TypedBuilder;
+    /// let builder = TypedBuilder::<bool>::with_capacity(42);
+    /// ```
     pub fn with_capacity(capacity: usize) -> Self {
         Self(BuilderBackend::<T>::new(BuilderConfig::<T>::with_capacity(
             capacity,
@@ -36,24 +46,44 @@ where
 //
 impl<T: ArrayElement + ?Sized> TypedBuilder<T> {
     /// Create a new array builder with an explicit configuration
+    //
+    // TODO: Add a usage example with an element type which actually needs a nontrivial config
     pub fn with_config(config: BuilderConfig<T>) -> Self {
         Self(BuilderBackend::<T>::new(config))
     }
 
     /// Number of elements the array can hold without reallocating
     ///
+    /// ```rust
+    /// # use arrow_typing::TypedBuilder;
+    /// let builder = TypedBuilder::<bool>::with_capacity(42);
+    /// assert!(builder.capacity() >= 42);
+    /// ```
+    ///
     /// In the case of types that are internally stored as multiple columnar
     /// buffers, like structs or unions, a lower bound on the capacity of all
     /// underlying columns is returned.
+    //
+    // TODO: Example
     ///
-    /// In the case of arrays of lists, capacity is to be understood as the
-    /// number of sublists that the array can hold, not the cumulative number of
-    /// elements across all sublists.
+    /// In the case of arrays of lists, array capacity is to be understood as
+    /// the number of sublists that the array can hold, not the cumulative
+    /// number of elements across all sublists.
+    //
+    // TODO: Example
     pub fn capacity(&self) -> usize {
         self.0.capacity()
     }
 
     /// Append a single value into the builder
+    ///
+    /// ```rust
+    /// # use arrow_typing::TypedBuilder;
+    /// let mut builder = TypedBuilder::<u8>::new();
+    /// builder.push(123);
+    /// ```
+    //
+    // TODO: Example with a type where T::Value is less obvious
     ///
     /// For types with a complex internal structure, such element-wise insertion
     /// may be inefficient. Therefore, if you intend to insert many values, it
@@ -66,10 +96,53 @@ impl<T: ArrayElement + ?Sized> TypedBuilder<T> {
 
     /// Efficiently append multiple values into the builder
     ///
+    /// ```rust
+    /// # use arrow_typing::TypedBuilder;
+    /// let mut builder = TypedBuilder::<u32>::new();
+    /// builder.extend_from_slice(&[
+    ///     0xbaaaaaad,
+    ///     0xcafed00d,
+    ///     0xdead2bad,
+    ///     0xfacefeed,
+    ///     0xf0cacc1a,
+    /// ]);
+    /// ```
+    ///
     /// This operation is available for all element types that implement
-    /// [`SliceElement`]. See the documentation of this trait for more
-    /// information on what types you can expect for `T::Slice` and
-    /// `T::ExtendFromSliceResult`.
+    /// [`SliceElement`]. This trait, in turn, is implemented for types where
+    /// Arrow provides a bulk insertion mechanism which is more efficient than
+    /// calling `push()` in a loop.
+    ///
+    /// For simple types, `T::Slice` is just `&[T]`. But for efficiency reasons,
+    /// slices of more complex types will have a less obvious columnar layout
+    /// containing multiple inner Rust slices. For example, slices of options
+    /// are passed as [`OptionSlice`]s:
+    ///
+    /// ```rust
+    /// # use arrow_typing::{TypedBuilder, OptionSlice};
+    /// let mut builder = TypedBuilder::<Option<f32>>::new();
+    /// builder.extend_from_slice(OptionSlice {
+    ///     values: &[
+    ///         1.2,
+    ///         3.4,
+    ///         5.6,
+    ///     ],
+    ///     is_valid: &[
+    ///         true,
+    ///         false,
+    ///         true
+    ///     ]
+    /// })?;
+    /// # Ok::<_, anyhow::Error>(())
+    /// ```
+    ///
+    /// While extending from a simple Rust slice always succeeds, extending from
+    /// composite slice types like `OptionSlice` may fail if the inner subslices
+    /// have differing lengths. Accordingly, this method returns `()` when
+    /// `T::Slice` is a simple Rust slice type, but `Result<(), ArrowError>`
+    /// when `T::Slice` is a composite slice type.
+    //
+    // TODO: Add an example with structs?
     pub fn extend_from_slice(&mut self, s: T::Slice<'_>) -> T::ExtendFromSliceResult
     where
         T: SliceElement,
@@ -94,6 +167,16 @@ where
     /// where `T` is a [`SliceElement`]. Given a slice of `T`, it lets you do
     /// the optimized equivalent of calling `push(Some(value))` in a loop for
     /// each value inside of the slice.
+    ///
+    /// ```rust
+    /// # use arrow_typing::TypedBuilder;
+    /// let mut builder = TypedBuilder::<Option<f64>>::new();
+    /// builder.extend_from_value_slice(&[
+    ///     2.4,
+    ///     6.8,
+    ///     10.12,
+    /// ]);
+    /// ```
     pub fn extend_from_value_slice(&mut self, vs: T::Slice<'_>) -> T::ExtendFromSliceResult {
         self.0.extend_from_slice(vs)
     }
@@ -104,6 +187,12 @@ impl<T: ArrayElement + ?Sized> TypedBuilder<T> {
     ///
     /// This operation is available when T is a [nullable
     /// type](NullableElement), i.e. `Null` or `Option<T>`.
+    ///
+    /// ```rust
+    /// # use arrow_typing::{TypedBuilder, types::primitive::Null};
+    /// let mut builder = TypedBuilder::<Null>::new();
+    /// builder.extend_with_nulls(666);
+    /// ```
     pub fn extend_with_nulls(&mut self, n: usize)
     where
         T: NullableElement,
@@ -112,11 +201,27 @@ impl<T: ArrayElement + ?Sized> TypedBuilder<T> {
     }
 
     /// Number of elements that were appended into this builder
+    ///
+    /// ```rust
+    /// # use arrow_typing::TypedBuilder;
+    /// let mut builder = TypedBuilder::<bool>::new();
+    /// assert_eq!(builder.len(), 0);
+    /// builder.push(true);
+    /// assert_eq!(builder.len(), 1);
+    /// ```
     pub fn len(&self) -> usize {
         self.0.len()
     }
 
     /// Truth that no elements were appended into this builder
+    ///
+    /// ```rust
+    /// # use arrow_typing::TypedBuilder;
+    /// let mut builder = TypedBuilder::<i32>::new();
+    /// assert!(builder.is_empty());
+    /// builder.push(42);
+    /// assert!(!builder.is_empty());
+    /// ```
     pub fn is_empty(&self) -> bool {
         self.0.is_empty()
     }
@@ -134,6 +239,29 @@ where
     ///
     /// This operation is only available on `TypedBuilder`s of optional `bool`s,
     /// [primitive types](PrimitiveType), bytes and strings.
+    ///
+    /// It may return `None` when all elements are known to be valid. Otherwise,
+    /// it will return a `&[bool]`-like [`ValiditySlice`] which can be used to
+    /// check which elements are valid.
+    ///
+    /// ```rust
+    /// # use arrow_typing::{TypedBuilder, OptionSlice};
+    /// let mut builder = TypedBuilder::<Option<f32>>::new();
+    /// let validity: &[bool] = &[true, false, true];
+    /// builder.extend_from_slice(OptionSlice {
+    ///     values: &[
+    ///         1.2,
+    ///         3.4,
+    ///         5.6,
+    ///     ],
+    ///     is_valid: validity
+    /// })?;
+    /// assert_eq!(
+    ///     builder.validity_slice().expect("not all elements are valid"),
+    ///     validity
+    /// );
+    /// # Ok::<_, anyhow::Error>(())
+    /// ```
     pub fn validity_slice(&self) -> Option<ValiditySlice<'_>> {
         use backend::ValiditySlice;
         self.0
@@ -182,7 +310,7 @@ where
         Self::default()
     }
 
-    /// Builder configuration with allocated room for `capacity` elements
+    /// Builder configuration with space for at least `capacity` elements
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
             capacity: Some(capacity),
